a0d08401a92241b606049be734d906d6a6c582db
Leticia Fontoura <leticia.fontoura@live.com>
2021-07-14 21:02:41 -0300

    ID created

--- index.ts
+++ index.ts
@@ -1,21 +1,60 @@
 interface Product {
-    name: string,
-    expireDate: Date,
-    status: ProductState
+    name: string;
+    expireDate: Date;
+    status: ProductState;
 }
+
+interface Identified {
+    id: number;
+}
+
+type IdentifiedProduct = Product & Identified;
 
 enum ProductState {
     OPEN = "open",
     CLOSED = "closed",
-    NOT_APPLICABLE = "n/a"
+    NOT_APPLICABLE = "n/a",
 }
 
 const p: Product = {
     name: "pao",
     expireDate: new Date(),
-    status: ProductState.CLOSED
+    status: ProductState.CLOSED,
+};
+
+const b: Product = {
+    name: "batata",
+    expireDate: new Date(),
+    status: ProductState.OPEN,
 }
 
-console.log(p)
+console.log(p);
 
-const products: Array<Product> = []
+const products: Array<IdentifiedProduct> = [];
+let id: number = 0;
+
+function add(product: Product) {
+    const nextId = id;
+    const identifiedProduct: IdentifiedProduct = {
+        // name: product.name,
+        // expireDate: product.expireDate,
+        // status: product.status,
+        // id: id
+        //
+        // the code above is the same as the code below
+        
+        ...product,
+        id: nextId
+    }
+    
+    products.push(identifiedProduct);
+    id = id + 1;
+
+    return identifiedProduct;
+}
+
+
+const idP = add(p);
+console.log(idP);
+add(b);
+
