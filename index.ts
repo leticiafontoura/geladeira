ddd835a35607498df61aeeba27822893a812c808
Leticia Fontoura <leticia.fontoura@live.com>
2021-07-21 18:02:29 -0300

    create delete and get methods

--- index.ts
+++ index.ts
@@ -27,8 +27,22 @@ const b: Product = {
     expireDate: new Date(),
     status: ProductState.OPEN,
 }
+const v:Product = {
+    name: "vinho", expireDate: new Date(), status: ProductState.CLOSED
+}
 
-console.log(p);
+const l:Product = {
+    name: "leite",
+    expireDate: new Date(),
+    status: ProductState.CLOSED
+}
+
+const q:Product = {
+    name: "queijo",
+    expireDate: new Date(),
+    status: ProductState.OPEN
+}
+// console.log(p);
 
 const products: Array<IdentifiedProduct> = [];
 let id: number = 0;
@@ -48,13 +62,57 @@ function add(product: Product) {
     }
     
     products.push(identifiedProduct);
-    id = id + 1;
+    id = id + 2;
 
     return identifiedProduct;
 }
 
 
 const idP = add(p);
-console.log(idP);
-add(b);
+const idB = add(b);
+const idV = add(v);
+const idL = add(l);
+const idQ = add(q);
 
+function getProduct(n: number) {
+    let productFound = products.find(product => {
+        return product.id === n;
+    })
+
+    return productFound;
+}
+
+function deleteProduct(n: number) {
+    if (products.find(product => {
+        return product.id === n;
+    })) {
+        const removeItem = products.findIndex( product => product.id === n);
+    products.splice(removeItem, 1);
+    return products;
+    }
+    else {
+        return products;
+    }
+}
+
+function updateItem(n: number, newProductInfo: Product) {
+    const indexOfProduct = products.findIndex( product => product.id === n);
+
+    products[indexOfProduct].name = newProductInfo.name;
+    products[indexOfProduct].expireDate = newProductInfo.expireDate;
+    products[indexOfProduct].status = newProductInfo.status;
+
+    return products;
+}
+
+const vinagre: Product = {
+    name: "vinagre",
+    expireDate: new Date(),
+    status: ProductState.OPEN
+}
+
+console.log(products);
+console.log(getProduct(6));
+console.log(deleteProduct(6));
+console.log(updateItem(4, vinagre));
+console.log(products);
