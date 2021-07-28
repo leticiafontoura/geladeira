97d148a0a2018b974db3e6bbc31a9eb20323d341
Leticia Fontoura <leticia.fontoura@live.com>
2021-07-28 20:53:13 -0300

    refactor code and change return types

--- index.ts
+++ index.ts
@@ -16,6 +16,62 @@ enum ProductState {
     NOT_APPLICABLE = "n/a",
 }
 
+const products: Array<IdentifiedProduct> = [];
+let id: number = 0;
+
+function addProduct(product: Product) {
+    const nextId = id;
+    const identifiedProduct: IdentifiedProduct = {
+        // name: product.name,
+        // expireDate: product.expireDate,
+        // status: product.status,
+        // id: id
+        //
+        // the code above is the same as the code below
+
+        ...product,
+        id: nextId,
+    };
+
+    products.push(identifiedProduct);
+    id = id + 1;
+
+    return identifiedProduct;
+}
+
+function getProduct(n: number): IdentifiedProduct | undefined {
+    return products.find((product) => product.id === n);
+}
+
+function deleteProduct(n: number): IdentifiedProduct | undefined {
+    const indexOfProductToRemove = products.findIndex((product) => product.id === n);
+
+    if (indexOfProductToRemove === -1) {
+        return undefined;
+    }
+
+    const productToRemove = products[indexOfProductToRemove];
+
+    products.splice(indexOfProductToRemove, 1);
+    return productToRemove;
+}
+
+function updateProduct(n: number, productToMerge: Product): IdentifiedProduct | undefined {
+    const productIndexToUpdate = products.findIndex((product) => product.id === n);
+    if (productIndexToUpdate === -1) {
+        return undefined;
+    }
+
+    products[productIndexToUpdate].name = productToMerge.name;
+    products[productIndexToUpdate].expireDate = productToMerge.expireDate;
+    products[productIndexToUpdate].status = productToMerge.status;
+
+    return products[productIndexToUpdate];
+}
+
+//
+// MAIN
+
 const p: Product = {
     name: "pao",
     expireDate: new Date(),
@@ -26,93 +82,40 @@ const b: Product = {
     name: "batata",
     expireDate: new Date(),
     status: ProductState.OPEN,
-}
-const v:Product = {
-    name: "vinho", expireDate: new Date(), status: ProductState.CLOSED
-}
+};
+const v: Product = {
+    name: "vinho",
+    expireDate: new Date(),
+    status: ProductState.CLOSED,
+};
 
-const l:Product = {
+const l: Product = {
     name: "leite",
     expireDate: new Date(),
-    status: ProductState.CLOSED
-}
+    status: ProductState.CLOSED,
+};
 
-const q:Product = {
+const q: Product = {
     name: "queijo",
     expireDate: new Date(),
-    status: ProductState.OPEN
-}
-// console.log(p);
+    status: ProductState.OPEN,
+};
 
-const products: Array<IdentifiedProduct> = [];
-let id: number = 0;
-
-function add(product: Product) {
-    const nextId = id;
-    const identifiedProduct: IdentifiedProduct = {
-        // name: product.name,
-        // expireDate: product.expireDate,
-        // status: product.status,
-        // id: id
-        //
-        // the code above is the same as the code below
-        
-        ...product,
-        id: nextId
-    }
-    
-    products.push(identifiedProduct);
-    id = id + 2;
-
-    return identifiedProduct;
-}
-
-
-const idP = add(p);
-const idB = add(b);
-const idV = add(v);
-const idL = add(l);
-const idQ = add(q);
-
-function getProduct(n: number) {
-    let productFound = products.find(product => {
-        return product.id === n;
-    })
-
-    return productFound;
-}
-
-function deleteProduct(n: number) {
-    if (products.find(product => {
-        return product.id === n;
-    })) {
-        const removeItem = products.findIndex( product => product.id === n);
-    products.splice(removeItem, 1);
-    return products;
-    }
-    else {
-        return products;
-    }
-}
-
-function updateItem(n: number, newProductInfo: Product) {
-    const indexOfProduct = products.findIndex( product => product.id === n);
-
-    products[indexOfProduct].name = newProductInfo.name;
-    products[indexOfProduct].expireDate = newProductInfo.expireDate;
-    products[indexOfProduct].status = newProductInfo.status;
-
-    return products;
-}
+const idP = addProduct(p);
+const idB = addProduct(b);
+const idV = addProduct(v);
+const idL = addProduct(l);
+const idQ = addProduct(q);
 
 const vinagre: Product = {
     name: "vinagre",
     expireDate: new Date(),
-    status: ProductState.OPEN
-}
+    status: ProductState.OPEN,
+};
 
 console.log(products);
 console.log(getProduct(6));
 console.log(deleteProduct(6));
-console.log(updateItem(4, vinagre));
+console.log(updateProduct(4, vinagre));
+updateProduct(4, vinagre);
 console.log(products);
